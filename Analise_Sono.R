caea8f89bfe805827ad82c2393fb9d8315d3a11c
Elvis Monteiro <93358171+ElvisRock@users.noreply.github.com>
2023-10-23 21:29:31 -0300

    Add files via upload

---
+++ Analise_Sono.R
@@ -0,0 +1,31 @@
+# A eficiência e duração do sono
+
+
+# Pacote lubridate
+?lubridate
+
+# Instala a ferramenta
+install.packages("lubridate")
+
+# Import da biblioteca
+require(lubridate)
+
+# Dataframe de datas
+sono <- data.frame(bed.time = ymd_hms("2013-09-01 23:05:24", "2013-09-02 22:51:09", 
+                                      "2013-09-04 00:09:16", "2013-09-04 23:43:31", "2013-09-06 00:17:41", "2013-09-06 22:42:27", 
+                                      "2013-09-08 00:22:27"), rise.time = ymd_hms("2013-09-02 08:03:29", "2013-09-03 07:34:21", 
+                                                                                  "2013-09-04 07:45:06", "2013-09-05 07:07:17", "2013-09-06 08:17:13", "2013-09-07 06:52:11", 
+                                                                                  "2013-09-08 07:15:19"), sleep.time = dhours(c(6.74, 7.92, 7.01, 6.23, 6.34, 7.42, 6.45)))
+sono
+sono$eficiencia <- round(sono$sleep.time/(sono$rise.time - sono$bed.time) * 100, 1)
+sono
+
+
+# Gerando um plot a partir de datas
+par(mar = c(5, 4, 4, 4))
+plot(round_date(sono$rise.time, "day"), sono$eficiencia, type = "o", col = "blue", xlab = "Manhã", ylab = NA)
+par(new = TRUE)
+plot(round_date(sono$rise.time, "day"), sono$sleep.time/3600, type = "o", col = "red", axes = FALSE, ylab = NA, xlab = NA)
+axis(side = 4)
+mtext(side = 4, line = 2.5, col = "red", "Duração do Sono")
+mtext(side = 2, line = 2.5, col = "blue", "Eficiência do Sono")
